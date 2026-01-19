3f4ea9a4e68693311aff6efedebb747b87122aa6
Blackfox-Thane <blackshadowxv15@gmail.com>
2026-01-19 15:19:10 -0500

    First Commit NixOS Configs

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		disko = {
+			url = "github:nix-community/disko";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		mango = {
+			url = "github:DreamMaoMao/mango";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = inputs@{ self, nixpkgs, disko, home-manager, mango, ... }: {
+		nixosConfigurations.nogi-nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./configuration.nix
+					inputs.disko.nixosModules.disko
+					home-manager.nixosModules.home-manager
+					mango.nixosModules.mango
+					{
+						home-manager = {
+							useGlobalPkgs = true;
+							useUserPackages = true;
+							users.thane = import ./home.nix;
+						};
+						programs.mango.enable = true;
+					}
+			];
+		};
+	};
+}
