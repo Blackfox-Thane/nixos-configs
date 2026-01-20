3a25fc73d120cb1c6326e2f0382c2fa9049dbcc2
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-01-20 03:28:57 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 		disko = {
 			url = "github:nix-community/disko";
 			inputs.nixpkgs.follows = "nixpkgs";
