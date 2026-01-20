866ec56e659e76aff45ea2c2283a73803643429d
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-01-20 02:26:55 -0500

    Update disko-config.nix

--- disko-config.nix
+++ disko-config.nix
@@ -11,7 +11,7 @@
 							priority = 1;
 							label = "boot";
 							name = "ESP";
-							size = "1G";
+							size = "512G";
 							type = "EF00";
 							content = {
 								type = "filesystem";
