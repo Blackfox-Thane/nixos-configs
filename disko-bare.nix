9b3b9742cc03af0b53ec62101c3427ce9079e7e7
BlackFox-Thane <blackshadowxv15@gmail.com>
2026-01-22 02:09:21 -0500

    nixos files

---
+++ disko-bare.nix
@@ -0,0 +1,52 @@
+{
+  disko.devices = {
+	disk = {
+	  main = {
+		device = "/dev/nvme0n1";
+		type = "disk";
+		content = {
+		  type = "gpt";
+		  partitions = {
+			ESP = {
+			  priority = 1;
+			  label = "boot";
+			  name = "ESP";
+			  size = "512M";
+			  type = "EF00";
+			  content = {
+				type = "filesystem";
+				format = "vfat";
+				mountpoint = "/boot";
+				mountOptions = [ "umask=0077" ];
+			  };
+			};
+			root = {
+			  size = "100%";
+			  content = {
+				type = "btrfs";
+				extraArgs = [ "-f" ];
+				subvolumes = {
+				  "/rootfs" = {
+					mountpoint = "/";
+				  };
+				  "/home" = {
+					mountOptions = [ "compress=zstd" ];
+					mountpoint = "/home";
+				  };
+				  "/nix" = {
+					mountOptions = [ "compress=zstd" "noatime" ];
+					mountpoint = "/nix";
+				  };
+				  "/swap" = {
+					mountpoint = "/.swapvol";
+					swap.swapfile.size = "8G";
+				  };
+				};
+			  };
+			};
+		  };
+		};
+	  };
+	};
+  };
+}
---
+++ home.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }:
+
+let
+dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+
+configs = {
+	alacritty = "alacritty";
+	kitty = "kitty";
+	mango = "mango";
+	nvim = "nvim";
+	fuzzel = "fuzzel";
+	waybar = "waybar";
+	yazi = "yazi";
+	zsh = "zsh";
+};
+in
+
+{
+	home.username = "thane";
+	home.homeDirectory = "/home/thane";
+	home.stateVersion = "25.11";
+	programs.git = {
+		enable = true;
+		settings = {
+			user = {
+				name = "Blackfox-Thane";
+				email = "blackshadowxv15@gmail.com";
+			};
+		init.defaultBranch = "main";
+		};
+	};
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			config-nix = "/usr/bin/git --git-dir=$HOME/DotfilesBare/nixos-bare --work-tree=$HOME/DotfilesBare";
+			nrs = "sudo nixos-rebuild switch --flake $HOME/nixos-dotfiles#nogi-nixos";
+		};
+	};
+	home.packages = with pkgs; [
+		neovim
+		fuzzel
+		alacritty
+		kitty
+		mangowc
+	];
+
+	xdg.configFile = builtins.mapAttrs
+		(name: subpath: {
+		 source = create_symlink "${dotfiles}/${subpath}";
+		 recursive = true;
+		 })
+	configs;
+
+
+}
