3c0f71dd230e74f5ce1fe109d880d1c1b2839d5c
BlackFox-Thane <blackshadowxv15@gmail.com>
2026-02-03 01:17:46 -0500

    Updates

--- flake.nix
+++ flake.nix
@@ -19,33 +19,25 @@
   };
 
   outputs = inputs@{ self, nixpkgs, disko, home-manager, mango, ... }:
-	let 
-	  lib = nixpkgs.lib;
+	nixosConfigurations.nogitsune = lib.nixosSystem {
 	  system = "x86_64-linux";
-	  pkgs = nixpkgs.legacyPackages.${system};
-	in {
-	  nixosConfigurations = {
-		nogitsune = lib.nixosSystem {
-		  inherit system;
-		  modules = [
-			./configuration.nix
-			disko.nixosModules.disko
-			mango.nixosModules.mango
-			home-manager.nixosModules.home-manager
-			{
-			  home-manager = {
-				useGlobalPkgs = true;
-				useUserPackages = true;
-				users.thane = import ./home.nix;
-				backupFileExtension = "backup";
-			  };
+	  modules = [
+		./configuration.nix
+		disko.nixosModules.disko
+		mango.nixosModules.mango
+		home-manager.nixosModules.home-manager
+		{
+		  home-manager = {
+			useGlobalPkgs = true;
+			useUserPackages = true;
+			users.thane = import ./home.nix;
+			backupFileExtension = "backup";
+		  };
 
-			  programs.mango = {
-				enable = true;
-			  };
-			}
-		  ];
-		};
-	  };
+		  programs.mango = {
+			enable = true;
+		  };
+		}
+	  ];
 	};
 }
